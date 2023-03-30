4706cdbac71a3ff41b1e80378c662607e7face9b
yutarou12 <yutarou12@syutarou.xyz>
2023-03-30 23:04:33 +0900

    [Add] キャラクターを選択する所まで完成

--- cogs/PartyCard.py
+++ cogs/PartyCard.py
@@ -36,7 +36,7 @@ class PartyCard(commands.Cog):
                     player = j.get("Player")
                     all_data = j.get("AllData")
                 else:
-                    return await interaction.followup.send(content='取得できませんでした¥nUIDが間違っていないか確認してください')
+                    return await interaction.followup.send(content='取得できませんでした\nUIDが間違っていないか確認してください')
 
         first_embed = discord.Embed(title=player["Name"])
         if player["Signature"]:
@@ -49,7 +49,9 @@ class PartyCard(commands.Cog):
             first_embed.set_image(url=f'https://enka.network/ui/{player["NameCard"]}.png')
 
         view = MainCharacterView()
-        character_select = MainCharacterSelect()
+        character_select = MainCharacterSelect(data={"AllData": all_data, "Player": player})
+
+        view.add_item(discord.ui.Button(style=discord.ButtonStyle.url, url='https://enka.network', label='Enka Network'))
 
         if player["showAvatarInfo"]:
             for i, chara in enumerate(player["showAvatarInfo"]):
@@ -57,7 +59,7 @@ class PartyCard(commands.Cog):
                 level = chara["level"]
                 character_select.add_option(label=name, description=f'Lv{level}', value=f'{i}')
         else:
-            character_select.add_option(label='取得できません')
+            character_select.add_option(label='取得できません', value='404')
         view.add_item(character_select)
 
         await interaction.followup.send(embed=first_embed, view=view)
@@ -69,30 +71,33 @@ class MainCharacterView(discord.ui.View):
         self.timeout = 120
         self.value = None
 
-    @discord.ui.button(label='Enka Network', style=discord.ButtonStyle.url)
-    async def url_button(self):
-        return
-
     @discord.ui.button(label='生成をキャンセル', style=discord.ButtonStyle.red)
     async def cancel_button(self, interaction: discord.Interaction, button):
         self.stop()
         self.value = True
+        return await interaction.response.edit_message(view=None)
 
 
 class MainCharacterSelect(discord.ui.Select):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, data: dict, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.placeholder = "メインキャラクターを選択"
+        self.data = data
 
     async def callback(self, interaction: discord.Interaction):
+        player = self.data.get('Player')
+        main_character = int(self.values[0])
+        if main_character == 404:
+            self.view.stop()
+            return await interaction.response.edit_message(view=None)
 
-        first_embed = discord.Embed(title='Step１：スコア換算基準を選択',
+        first_embed = discord.Embed(title='Step1：スコア換算基準を選択',
                                     description='聖遺物のスコアの換算基準を選択してください。')
         first_embed.set_author(name=player.get('Name'), url=f'https://enka.network/ui/{player["ProfilePicture"]}.png')
         first_embed.set_footer(text='原神パーティー編成画像生成')
 
-        view = StepOneView()
-        step_first = StepFirstSelect()
+        view = discord.ui.View()
+        step_first = StepFirstSelect(data=self.data, main_chara=main_character)
         for t in ['HP', '攻撃', '防御', 'チャージ', '元素熟知', 'キャンセル']:
             if t == 'キャンセル':
                 step_first.add_option(label=t, description='画像の生成をキャンセルします')
@@ -100,20 +105,61 @@ class MainCharacterSelect(discord.ui.Select):
                 step_first.add_option(label=t)
         view.add_item(step_first)
 
-        await interaction.followup.send(embed=first_embed, view=view)
-
-        return
-
-
-class StepOneView(discord.ui.View):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        return await interaction.response.edit_message(embed=first_embed, view=view)
 
 
 class StepFirstSelect(discord.ui.Select):
-    def __init__(self):
+    def __init__(self, data: dict, main_chara: int):
         super().__init__()
         self.placeholder = "スコア基準を選択"
+        self.data = data
+        self.main_character = main_chara
+
+    async def callback(self, interaction: discord.Interaction):
+        print(self.values)
+        if self.values[0] == 'キャンセル':
+            self.view.stop()
+            return await interaction.response.edit_message(view=None)
+        else:
+            state = self.values[0]
+            player = self.data.get('Player')
+
+            embed = discord.Embed(title='Step2：キャラクターを選択',
+                                        description='パーティーに入れるキャラクターを3人選択してください。')
+            embed.add_field(name='メインキャラクター',
+                            value=f'{fetch_character(str(player["showAvatarInfo"][self.main_character]["avatarId"]))}')
+            embed.set_author(name=player.get('Name'), url=f'https://enka.network/ui/{player["ProfilePicture"]}.png')
+            embed.set_footer(text='原神パーティー編成画像生成')
+
+            view = discord.ui.View()
+            select = StepSecondSelect(state=state, data=self.data, main_chara=self.main_character)
+
+            if player["showAvatarInfo"]:
+                for i, chara in enumerate(player["showAvatarInfo"]):
+                    name = fetch_character(str(chara["avatarId"]))
+                    level = chara["level"]
+                    select.add_option(label=name, description=f'Lv{level}', value=f'{i}')
+            else:
+                select.add_option(label='取得できません', value='404')
+
+            view.add_item(select)
+
+            return await interaction.response.edit_message(view=view, embed=embed)
+
+
+class StepSecondSelect(discord.ui.Select):
+    def __init__(self, state: str, data: dict, main_chara: int):
+        super().__init__()
+        self.placeholder = "キャラクターを選択"
+        self.max_values = 3
+        self.min_values = 3
+        self.state = state
+        self.data = data
+        self.main_character = main_chara
 
     async def callback(self, interaction: discord.Interaction):
         return
+
+
+async def setup(bot):
+    await bot.add_cog(PartyCard(bot))

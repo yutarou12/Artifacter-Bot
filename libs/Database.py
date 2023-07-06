3c853e3a351d4fb8210e1b8e6ab1ae071ea0dcbb
yutarou12 <sasaki.yuutarou12@gmail.com>
2023-07-06 10:50:14 +0900

    [Fix] Databaseの変更

--- libs/Database.py
+++ libs/Database.py
@@ -2,126 +2,113 @@ import os
 import logging
 from functools import wraps
 
-import pymysql.cursors
+import asyncpg
 
 
 class ProductionDatabase:
 
     def __init__(self):
-        self.connection = None
+        self.pool = None
 
-    def setup(self):
-        self.connection = pymysql.connect(host=os.getenv('MYSQL_HOST_NAME'),
-                                          user=os.getenv('MYSQL_USER'),
-                                          password=os.getenv('MYSQL_PASSWORD'),
-                                          database=os.getenv('MYSQL_DATABASE_NAME'),
-                                          cursorclass=pymysql.cursors.DictCursor)
+    async def setup(self):
+        self.pool = await asyncpg.create_pool(f"postgresql://{os.getenv('POSTGRESQL_USER')}:{os.getenv('POSTGRESQL_PASSWORD')}@{os.getenv('POSTGRESQL_HOST_NAME')}:{os.getenv('POSTGRESQL_PORT')}/{os.getenv('POSTGRESQL_DATABASE_NAME')}")
 
-        with self.connection.cursor() as cursor:
-            cursor.execute(
-                "CREATE TABLE IF NOT EXISTS `user_uid` (`user_id` bigint NOT NULL, `uid` char(10) NOT NULL, PRIMARY KEY (`user_id`))")
-            cursor.execute(
-                "CREATE TABLE IF NOT EXISTS `premium_guild` (`guild_id` bigint NOT NULL, PRIMARY KEY (`guild_id`))")
-        self.connection.commit()
+        with self.pool.acquire() as conn:
+            await conn.execute(
+                "CREATE TABLE IF NOT EXISTS user_uid (user_id bigint NOT NULL PRIMARY KEY, uid char(10) NOT NULL)")
+            await conn.execute(
+                "CREATE TABLE IF NOT EXISTS premium_guild (guild_id bigint NOT NULL, PRIMARY KEY (guild_id))")
 
-        return self.connection
+        return self.pool
 
     def check_connection(func):
-        def inner(self, *args, **kwargs):
-            self.connection = self.connection or self.setup()
-            print(self.connection)
-            return func(self, *args, **kwargs)
+        @wraps(func)
+        async def inner(self, *args, **kwargs):
+            self.pool = self.pool or await self.setup()
+            return await func(self, *args, **kwargs)
+
         return inner
 
     @check_connection
-    def execute(self, sql):
-        with self.connection.cursor() as cursor:
-            cursor.execute(sql)
+    async def execute(self, sql):
+        async with self.pool.acquire() as con:
+            await con.execute(sql)
 
     @check_connection
-    def fetch(self, sql):
-        with self.connection.cursor() as cursor:
-            data = cursor.fetchone(sql)
+    async def fetch(self, sql):
+        async with self.pool.acquire() as con:
+            data = await con.fetch(sql)
         return data
 
     @check_connection
-    def get_uid_from_user(self, user_id: int):
-        with self.connection.cursor() as cursor:
-            cursor.excute('SELECT `uid` FROM `user_uid` WHERE `user_id`=%s', (user_id,))
-            result = cursor.fetchone()
+    async def get_uid_from_user(self, user_id: int):
+        async with self.pool.acquire() as con:
+            data = await con.fetch('SELECT uid FROM user_uid WHERE user_id=$1', (user_id,))
+            result = data
         return result
 
     @check_connection
-    def get_user_from_uid(self, uid: str):
-        with self.connection.cursor() as cursor:
-            cursor.excute('SELECT `user_id` FROM `user_uid` WHERE `uid`=%s', (uid,))
-            result = cursor.fetchone()
+    async def get_user_from_uid(self, uid: str):
+        async with self.pool.acquire() as con:
+            data = await con.fetch('SELECT user_id FROM user_uid WHERE uid=$1', (uid,))
+            result = data
         return result
 
     @check_connection
-    def add_user_uid(self, user_id: int, uid: str):
-        with self.connection.cursor() as cursor:
-            cursor.excute("INSERT INTO `user_uid` (`user_id`, `uid`) VALUES (%s, %s)", (user_id, uid))
-        self.connection.commit()
+    async def add_user_uid(self, user_id: int, uid: str):
+        async with self.pool.acquire() as con:
+            await con.excute("INSERT INTO user_uid (user_id, uid) VALUES ($1, $2)", (user_id, uid))
 
     @check_connection
-    def remove_user_uid(self, user_id: int, uid: str):
-        with self.connection.cursor() as cursor:
-            cursor.excute("DELETE FROM `user_uid` WHERE `user_id`=%s AND `uid`=%s", (user_id, uid))
-        self.connection.commit()
+    async def remove_user_uid(self, user_id: int, uid: str):
+        async with self.pool.acquire() as con:
+            await con.excute("DELETE FROM user_uid WHERE user_id=$1 AND uid=$2", (user_id, uid))
 
     @check_connection
-    def get_premium_guild_list(self):
-        with self.connection as con:
-            with con.cursor() as cursor:
-                print('---------------get--------')
-                print(cursor)
-                print(self.connection)
-                sql = "SELECT * FROM `premium_guild`"
-                cursor.excute(sql)
-                result = cursor.fetchall()
+    async def get_premium_guild_list(self):
+        async with self.pool.acquire() as con:
+            data = await con.fetch("SELECT * FROM premium_guild")
+            result = data
             return result
 
     @check_connection
-    def add_premium_guild(self, guild_id: int):
-        with self.connection.cursor() as cursor:
-            cursor.excute("INSERT INTO premium_guild (guild_id)  VALUES (%s)", (guild_id,))
-        self.connection.commit()
+    async def add_premium_guild(self, guild_id: int):
+        async with self.pool.acquire() as con:
+            await con.excute("INSERT INTO premium_guild (guild_id)  VALUES ($1)", (guild_id,))
 
     @check_connection
-    def remove_premium_guild(self, guild_id: int):
-        with self.connection.cursor() as cursor:
-            cursor.excute("DELETE FROM premium_guild WHERE guild_id=%s", (guild_id, ))
-        self.connection.commit()
+    async def remove_premium_guild(self, guild_id: int):
+        async with self.pool.acquire() as con:
+            await con.excute("DELETE FROM premium_guild WHERE guild_id=$1", (guild_id, ))
 
 
 class DebugDatabase(ProductionDatabase):
 
-    def execute(self, sql):
+    async def execute(self, sql):
         logging.info(f"executing sql: {sql}")
 
-    def fetch(self, sql):
+    async def fetch(self, sql):
         logging.info(f"fetching by sql: {sql}")
 
-    def get_uid_from_user(self, user_id: int):
+    async def get_uid_from_user(self, user_id: int):
         return '859015814'
 
-    def get_user_from_uid(self, uid: str):
+    async def get_user_from_uid(self, uid: str):
         return 534994298827964416
 
-    def add_user_uid(self, user_id: int, uid: str):
+    async def add_user_uid(self, user_id: int, uid: str):
         pass
 
-    def remove_user_uid(self, user_id: int, uid: str):
+    async def remove_user_uid(self, user_id: int, uid: str):
         pass
 
-    def get_premium_guid_list(self):
+    async def get_premium_guid_list(self):
         return []
 
-    def add_premium_guid(self, guild_id: int):
+    async def add_premium_guid(self, guild_id: int):
         pass
 
-    def remove_premium_guid(self, guild_id: int):
+    async def remove_premium_guid(self, guild_id: int):
         pass
 
 

8f2c077f8460dbe9e31a57bf52e4ad0989d091c0
yutarou12 <yutarou12@syutarou.xyz>
2024-10-20 12:21:51 +0900

    [Change] env抜けと週ボスリスト改善

--- cogs/Update.py
+++ cogs/Update.py
@@ -5,6 +5,8 @@ import shutil
 import requests
 from discord.ext import commands
 
+from libs.env import API_HOST_NAME
+
 
 def add_file(resource: str, version: str, write_data: dict):
     if os.path.exists(f'./data/{resource}.json'):
@@ -26,7 +28,7 @@ class Update(commands.Cog):
         if not version:
             return await ctx.send('No version')
 
-        res_1 = requests.get(f'http://{os.getenv("API_HOST_NAME")}:8080/api/update/{version}/fetch')
+        res_1 = requests.get(f'http://{API_HOST_NAME}:8080/api/update/{version}/fetch')
         if res_1.status_code == 404:
             return await ctx.send('404')
         elif res_1.status_code == 200:
@@ -35,7 +37,7 @@ class Update(commands.Cog):
             return await ctx.send(f'{res_1.status_code}')
 
         for file in ["lang", "chara", "pfps", "namecard"]:
-            res_2 = requests.get(f'http://{os.getenv("API_HOST_NAME")}:8080/api/update/{version}/{file}')
+            res_2 = requests.get(f'http://{API_HOST_NAME}:8080/api/update/{version}/{file}')
             res_2.encoding = res_2.apparent_encoding
             if file == 'lang':
                 res_data = res_2.json()
@@ -46,7 +48,7 @@ class Update(commands.Cog):
             print(res_2.encoding)
             print(f'完了 - {file}')
 
-        res_3 = requests.get(f'http://{os.getenv("API_HOST_NAME")}:8080/api/update/{version}/images')
+        res_3 = requests.get(f'http://{API_HOST_NAME}:8080/api/update/{version}/images')
         if res_3.status_code == 404:
             print(res_3.content)
             return await ctx.send(f'/api/update/{version}/images - 404')
